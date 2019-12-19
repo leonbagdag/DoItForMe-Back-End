0cd85bdc3e0875888b33df9e233f5d990fb9091b
Luis Alejandro Lucena <luis.lucena89@gmail.com>
2019-12-19 16:08:53 -0300

    tablas agregadas a a la bd.

--- src/models.py
+++ src/models.py
@@ -4,24 +4,24 @@ from datetime import datetime
 db = SQLAlchemy()
 
 # Join table between user and category
-user_category = db.Table('habilities', db.metadata,
+user_category = db.Table('user_category', db.metadata,
     db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
     db.Column("category_id", db.Integer, db.ForeignKey("category.id"))
 )
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    role = db.Column(db.String(10), default = 'client', nullable = False) # Role is client or admin
-    username = db.Column(db.String(80), unique = True, nullable = False)
-    email = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(30), nullable = False)
-    register_date = db.Column(db.DateTime, default = datetime.now, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(10), default='client', nullable=False) # Role is client or admin
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    register_date = db.Column(db.DateTime, default=datetime.now, nullable=False)
 
-    profile = db.relationship('Profile', back_populates='user', uselist = False, lazy = True) # 1 to 1 with profile
-    reviews = db.relationship('Review', back_populates='user', lazy = True) # 1 to many with reviews
-    categories = db.relationship('Category', secondary=user_category, back_populates='users', lazy = True) #many to many with categories
-    offers = db.relationship('Offer', back_populates='user', lazy = True) # 1 to many with offers
-    services = db.relationship('Service_req', back_populates='user', lazy = True) # 1 to many with service_req
+    profile = db.relationship('Profile', back_populates='user', uselist=False, lazy=True) # 1 to 1 with profile
+    reviews = db.relationship('Review', back_populates='user', lazy=True) # 1 to many with reviews
+    categories = db.relationship('Category', secondary=user_category, back_populates='users', lazy=True) #many to many with categories
+    offers = db.relationship('Offer', back_populates='user', lazy=True) # 1 to many with offers
+    services = db.relationship('Service', back_populates='user', lazy = True) # 1 to many with service
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -30,17 +30,17 @@ class User(db.Model):
         return {
             "id": self.id,
             "username": self.username,
-            "email": self.email
-            "since": self.register_date.year
+            "email": self.email,
+            "since": self.register_date
         }
 
 class Category(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique = True, nullable = False)
     logo = db.Column(db.String(60), unique = True, nullable = False) #From Font-awsome
 
     users = db.relationship('User', secondary=user_category, back_populates='categories', lazy = True) #many to many with users
-    services = db.relationship('Service_req', back_populates='category', lazy = True) #1 to many with service_req
+    services = db.relationship('Service', back_populates='category', lazy = True) #1 to many with service
 
     def __repr__(self):
         return '<Category %r>' % self.name
@@ -53,7 +53,7 @@ class Category(db.Model):
         }
 
 class Profile(db.Model): # 1 to 1 rel with User
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(80))
     lname = db.Column(db.String(80))
     region = db.Column(db.String(80))
@@ -109,13 +109,13 @@ class Review(db.Model): #1 to many rel with User
 class Offer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     offer_body = db.Column(db.Text)
-    offer_status = db.Column(db.String(10), default = 'active', nullabe = False)
+    offer_status = db.Column(db.String(10), default = 'active', nullable = False)
     offer_date = db.Column(db.DateTime, default = datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # user offering a service
-    service_id = db.Column(db.Integer, db.ForeignKey('service_req.id'))
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
 
     user = db.relationship('User', back_populates = 'offers', lazy = True) # 1 to many with user
-    service = db.relationship('Service_req', back_populates = 'offers', lazy = True) #1 to many with service_req
+    service = db.relationship('Service', back_populates = 'offers', lazy = True) #1 to many with service
 
     def __repr__(self):
         return '<Offer %r>' % self.id
@@ -129,8 +129,8 @@ class Offer(db.Model):
             'provider': self.user_id
         }
 
-class Service_req(db.Model):
-    id = db.column(db.Integer, primary_key=True)
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     service_name = db.Column(db.String(60), nullable = False)
     description = db.Column(db.Text, nullable = False)
     provider_id = db.Column(db.Integer, default = 0, nullable = False) # user winner of the contract, from front-end. If 0 is an open service req
@@ -145,7 +145,7 @@ class Service_req(db.Model):
     contract = db.relationship('Contract', back_populates='service', uselist=False, lazy = True) #1 to 1 with Contract
 
     def __repr__(self):
-        return '<Service %r>' % self.id
+        return '<Service %r>' % self.service_name
 
     def serialize(self):
         return {
@@ -160,16 +160,16 @@ class Service_req(db.Model):
         }
 
 class Cotract(db.Model):
-    id = db.column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     contract_status = db.Column(db.String(10), default = 'active', nullable = False) # status options: active, paused, cancelled
     contract_date = db.Column(db.DateTime, default = datetime.now, nullable = False)
-    service_id = db.Column(db.Integer, db.ForeignKey('service_req.id'))
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
 
-    service = db.relationship('Service_req', back_populates='contract', lazy = True) #1 to 1 with service_req
+    service = db.relationship('Service', back_populates='contract', lazy = True) #1 to 1 with service
 
     
     def __repr__(self):
-        return '<Contact %r>' %self.id
+        return '<Contact %r>' % self.id
 
     def serialize(self):
         return {
