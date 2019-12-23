c6eca6c05d5a0b640e6d0e9afa405bb95482c3b4
Luis Alejandro Lucena <luis.lucena89@gmail.com>
2019-12-23 14:39:43 -0300

    se elimina carpeta migrations de git durante el desarrollo

--- src/models.py
+++ src/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 # Join table between user and category
 provider_category = db.Table('provider_catgory', db.metadata,
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("provider_id", db.Integer, db.ForeignKey("provider.id")),
     db.Column("category_id", db.Integer, db.ForeignKey("category.id"))
 )
 class User(db.Model):
@@ -17,8 +17,8 @@ class User(db.Model):
     password = db.Column(db.String(30), nullable=False)
     register_date = db.Column(db.DateTime, default=datetime.now, nullable=False)
 
-    provider = db.relationship('Provider', back_populates='user', uselist=False, lazy=True)
-    employer = db.relationship('Employer', back_populates='user', uselist=False, lazy=True)
+    provider = db.relationship('Provider', back_populates='user', uselist=False, lazy=True) # 1 to 1 with profile
+    employer = db.relationship('Employer', back_populates='user', uselist=False, lazy=True) # 1 to 1 with profile
     profile = db.relationship('Profile', back_populates='user', uselist=False, lazy=True) # 1 to 1 with profile
 
     def __repr__(self):
@@ -40,6 +40,9 @@ class Employer(db.Model):
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     score = db.Column(db.Float, default=0)
 
+    user = db.relationship('User', back_populates='employer', lazy=True)
+    contracts = db.relationship('Contract', back_populates='employer', lazy=True)
+
     def __repr__(self):
         return '<Employer %r>' % self.id
 
@@ -47,6 +50,7 @@ class Employer(db.Model):
         return {
             'id': self.id,
             'score': self.score,
+            'contracts': list(map(lambda x: x.serialize, self.contracts)),
         }
 
 class Provider(db.Model):
@@ -54,16 +58,19 @@ class Provider(db.Model):
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     score = db.Column(db.Float, default = 0)
 
+    user = db.relationship('User', back_populates='provider', lazy=True)
     categories = db.relationship('Category', secondary=provider_category, back_populates='providers', lazy=True) #many to many with categories
+    contracts = db.relationship('Contract', back_populates='provider', lazy=True)
 
     def __repr__(self):
-        return '<Provider %r', % self.id
+        return '<Provider %r>' % self.id
 
     def serialize(self):
         return {
             'id': self.id,
             'score': self.score,
             'categories': list(map(lambda x: x.serialize(), self.categories)),
+            'contracts': list(map(lambda x: x.serialize(), self.contracts)),
         }
 
 class Contract(db.Model):
@@ -72,6 +79,11 @@ class Contract(db.Model):
     contract_status = db.Column(db.String(10), default = 'active', nullable=False) # status options: active, paused, cancelled
     contract_start_date = db.Column(db.DateTime, default = datetime.now, nullable=False)
     contract_end_date = db.Column(db.DateTime)
+    employer_id = db.Column(db.Integer, db.ForeignKey('employer.id'))
+    provider_id = db.Column(db.Integer, db.ForeignKey('provider.id'))
+
+    employer = db.relationship('Employer', back_populates='contracts', lazy=True)
+    provider = db.relationship('Provider', back_populates='contracts', lazy=True)
     
     def __repr__(self):
         return '<Contract %r>' % self.id
@@ -81,7 +93,9 @@ class Contract(db.Model):
             'id': self.id,
             'status': self.contract_status,
             'start_date': self.contract_date,
-            'end_date': self.contract_end_date
+            'end_date': self.contract_end_date,
+            'employer_id': self.employer_id,
+            'provider_id': self.provider_id,
         }
 
 class Category(db.Model):
@@ -99,7 +113,7 @@ class Category(db.Model):
         return {
             'id': self.id,
             'name': self.name,
-            'logo': self.logo
+            'logo': self.logo,
         }
 
 class Profile(db.Model): # 1 to 1 rel with User
@@ -128,21 +142,4 @@ class Profile(db.Model): # 1 to 1 rel with User
             "comuna": self.comuna,
             "address1": self.address_1,
             "address2": self.address_2,
-        }
-
-class Review(db.Model): #1 to many rel with User
-    __tablename__ = 'review'
-    id = db.Column(db.Integer, primary_key=True)
-    review_body = db.Column(db.Text)
-    score = db.Column(db.Float, nullable=False)
-    contract_id = db.Column(db.Integer, nullable=False) #contract involving provider and employer, from front-end
-    from_user = db.Column(db.Integer, nullable=False) #user that make the review, from front-end
-
-    def serialize(self):
-        return {
-            "review_id": self.id,
-            "review": self.review_body,
-            "review_as": self.user_as
-            "score": self.score,
-            "contract": self.contract_id,
         }
